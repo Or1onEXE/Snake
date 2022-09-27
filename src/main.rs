abdf574cdae5858e04d2a3eeaf98515846b2927d
orion <aleksakhinnicolay@gmail.com>
2022-09-27 23:26:53 +0300

    ver_0.2.1

--- src/main.rs
+++ src/main.rs
@@ -35,10 +35,9 @@ struct Cell {
 struct Snake {
 	entity: Option<Entity>,
 	direction: Direction,
-	// i: Vec<usize>,
-	// j: Vec<usize>,
-	i: usize,
-	j: usize,
+	i: Vec<usize>,
+	j: Vec<usize>,
+	size: usize,
 	move_cooldown: Timer,
 }
 
@@ -55,8 +54,6 @@ struct Game {
 	board: Vec<Vec<Cell>>,
 	snake: Snake,
 	apple: Apple,
-	score: u32,
-	apples_eaten: u32,
 	camera_should_focus: Vec3,
 	camera_is_focus: Vec3,
 }
@@ -91,11 +88,11 @@ fn main() {
 		)
 		.add_system_set(SystemSet::on_exit(GameState::Playing).with_system(teardown))
 		.add_system_set(SystemSet::on_exit(GameState::GameOver).with_system(teardown))
-		// .add_system_set(SystemSet::on_enter(GameState::GameOver).with_system(display_score))
 		.add_system_set(SystemSet::on_update(GameState::GameOver).with_system(gameover_keyboard))
 		.add_system_set(
 			SystemSet::new()
-				.with_run_criteria(FixedTimestep::step(5.0))
+				.with_run_criteria(FixedTimestep::step(0.01))
+				.with_system(spawn_apple)
 		)
 		.add_system(bevy::window::close_on_esc)
 		.run();
@@ -124,9 +121,9 @@ fn setup(
 	asset_server: Res<AssetServer>,
 	mut game: ResMut<Game>
 ) {
-	game.score = 0;
-	game.snake.i = 5;
-	game.snake.j = 5;
+	game.snake.size = 1;
+	game.snake.i = vec!(BOARD_SIZE_I / 2);
+	game.snake.j = vec!(BOARD_SIZE_J / 2);
 	game.snake.direction = Direction::Down;
 	game.snake.move_cooldown = Timer::from_seconds(0.3, false);
 
@@ -163,17 +160,19 @@ fn setup(
 		.spawn_bundle(SceneBundle {
 			transform: Transform {
 				translation: Vec3::new(
-					game.snake.i as f32,
+					game.snake.i[0] as f32,
 					0_f32,
-					game.snake.j as f32,
+					game.snake.j[0] as f32,
 				),
 				..default()
 			},
-			scene: asset_server.load("models/red_cube.glb#Scene0"),
+			scene: asset_server.load("models/alien.glb#Scene0"),
 			..default()
 		})
 		.id()
 	);
+
+	game.apple.handle = asset_server.load("models/red_cube.glb#Scene0");
 
 	commands.spawn_bundle(
 		TextBundle::from_section(
@@ -248,6 +247,8 @@ fn move_snake(
 	mut transform: Query<&mut Transform>,
 	time: Res<Time>,
 ) {
+	if keyboard_input.just_pressed(KeyCode::Space) { game.snake.size += 1; }
+	
 	fn check(keyboard_input: Input<KeyCode>, key_code: KeyCode) -> bool {
 		if keyboard_input.pressed(key_code) || keyboard_input.just_pressed(key_code) 
 			{ return true }
@@ -271,42 +272,95 @@ fn move_snake(
 	if game.snake.move_cooldown.tick(time.delta()).finished() {
 		match game.snake.direction {
 			Direction::Up => {
-				if game.snake.i == BOARD_SIZE_I - 1 { game.snake.i = 0; }
-				else { game.snake.i += 1; }
+				if game.snake.i[0] == BOARD_SIZE_I - 1 { game.snake.i[0] = 0; }
+				else { game.snake.i[0] += 1; }
 			},
 			Direction::Down => {
-				if game.snake.i == 0 { game.snake.i = BOARD_SIZE_I - 1; }
-				else { game.snake.i -= 1; }
+				if game.snake.i[0] == 0 { game.snake.i[0] = BOARD_SIZE_I - 1; }
+				else { game.snake.i[0] -= 1; }
 			},
 			Direction::Right => {
-				if game.snake.j == BOARD_SIZE_J - 1 { game.snake.j = 0; }
-				else { game.snake.j += 1; }
+				if game.snake.j[0] == BOARD_SIZE_J - 1 { game.snake.j[0] = 0; }
+				else { game.snake.j[0] += 1; }
 			},
 			Direction::Left => {
-				if game.snake.j == 0 { game.snake.j = BOARD_SIZE_J - 1; }
-				else { game.snake.j -= 1; }
+				if game.snake.j[0] == 0 { game.snake.j[0] = BOARD_SIZE_J - 1; }
+				else { game.snake.j[0] -= 1; }
 			},
+		}
+
+		for i in 1..game.snake.size {
+
 		}
 		
 		game.snake.move_cooldown.reset();
 		*transform.get_mut(game.snake.entity.unwrap()).unwrap() = Transform {
 			translation: Vec3::new(
-				game.snake.i as f32,
-				game.board[game.snake.j][game.snake.i].height,
-				game.snake.j as f32,
+				game.snake.i[0] as f32,
+				game.board[game.snake.j[0]][game.snake.i[0]].height,
+				game.snake.j[0] as f32,
 			),
 			..default()
 		};
 	
 
 		if let Some(entity) = game.apple.entity {
-			if game.snake.i == game.apple.i && game.snake.j == game.apple.j {
-				game.score += 1;
+			if game.snake.i[0] == game.apple.i && game.snake.j[0] == game.apple.j {
+				game.snake.size += 1;
 				commands.entity(entity).despawn_recursive();
 				game.apple.entity = None;
 			}
 		}
 	}
+}
+
+fn spawn_apple(
+	mut state: ResMut<State<GameState>>,
+	mut game: ResMut<Game>,
+	mut commands: Commands,
+) {
+	if *state.current() != GameState::Playing || game.apple.entity != None {
+		return;
+	}
+
+	loop {
+		game.apple.i = rand::thread_rng().gen_range(0..BOARD_SIZE_I);
+		game.apple.j = rand::thread_rng().gen_range(0..BOARD_SIZE_J);
+		let mut flag: bool = true;
+		for k in 0..game.snake.size {
+			if game.apple.i == game.snake.i[k] && game.apple.j == game.snake.j[k] {
+				flag = false;
+			}
+		}
+		if flag { break }
+	}
+
+	game.apple.entity = Some(
+		commands.spawn_bundle(
+			SceneBundle {
+				transform: Transform::from_xyz(
+					game.apple.i as f32,
+					game.board[game.apple.i][game.apple.j].height + 0.2,
+					game.apple.j as f32,
+				),
+				scene: game.apple.handle.clone(),
+				..default()
+			}
+		)
+		.with_children(|children| {
+			children.spawn_bundle( PointLightBundle {
+				point_light: PointLight {
+					color: Color::rgb(1.0, 1.0, 0.0),
+					intensity: 1000.0,
+					range: 10.0,
+					..default()
+				},
+				transform: Transform::from_xyz(0.0, 2.0, 0.0),
+				..default()
+			});
+		})
+		.id(),
+	);
 }
 
 fn gameover_keyboard(
@@ -323,5 +377,5 @@ fn scoreboard_system(
 	mut query: Query<&mut Text>,
 ) {
 	let mut text = query.single_mut();
-	text.sections[0].value = format!("Score: {}", game.apples_eaten);
+	text.sections[0].value = format!("Score: {}", game.snake.size * 10 - 10);
 }
